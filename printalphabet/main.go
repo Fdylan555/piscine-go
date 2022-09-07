1b84fd92f488bcc2602959c157bd590b210f963a
WanekNia <nathan.wanecque@outlook.com>
2022-09-07 15:56:03 +0200

    ...

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-    "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func main() {
 
-    z01.PrintRune('\n')
+	for compteur := 97; compteur <= 122; compteur++ {
 
-    for compteur := 97; compteur <= 122; compteur ++ {
+		z01.PrintRune(rune(compteur))
 
-        z01.PrintRune(rune(compteur))
+	}
 
-
-    }
-
-	
+	z01.PrintRune('\n')
 }
