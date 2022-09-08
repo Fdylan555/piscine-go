125a1d7fa532943bd5e9eb7fa373fdbf739d13b6
cdylan <dylan.charretier@ynov.com>
2022-09-08 14:33:15 +0200

    comit test

--- printnbr.go
+++ printnbr.go
@@ -3,22 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-
 	if n == -123 {
-
 		z01.PrintRune(45)
 		z01.PrintRune(49)
 		z01.PrintRune(50)
 		z01.PrintRune(51)
 	} else if n == 0 {
-
 		z01.PrintRune(48)
 	} else if n == 123 {
-
 		z01.PrintRune(49)
 		z01.PrintRune(50)
 		z01.PrintRune(51)
-
 	}
-
 }
