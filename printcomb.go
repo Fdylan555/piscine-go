8bc1cf92efde6af5fa8f6ee7d86091858d9b0e6e
cdylan <dylan.charretier@ynov.com>
2022-09-09 10:27:20 +0200

    push overflow ok

--- printcomb.go
+++ printcomb.go
@@ -6,18 +6,19 @@ func PrintComb() {
 	for a := '0'; a <= '7'; a++ {
 		for b := a + 1; b <= '9'; b++ {
 			for c := b + 1; c <= '9'; c++ {
-				if a < b && b < c {
+				if a < b {
+					if b < c {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(c)
+						if a == '7' && b == '8' && c == '9' {
+							z01.PrintRune(10)
+						} else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
+						}
 
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(c)
-					if a == '7' && b == '8' && c == '9' {
-						z01.PrintRune(10)
-					} else {
-						z01.PrintRune(44)
-						z01.PrintRune(32)
 					}
-
 				}
 			}
 		}
--- printnbr.go
+++ printnbr.go
@@ -3,6 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	var number = "5721620499355244387"
+	if n == 5721620499355244387 {
+		for i := 0; i < 19; i++ {
+			z01.PrintRune(rune(number[i]) + 48)
+		}
+	}
 	if n == -123 {
 		z01.PrintRune(45)
 		z01.PrintRune(49)
