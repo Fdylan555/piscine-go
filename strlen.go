fc980f13281424e97eb70ee70eca6294d86ae2c9
cdylan <dylan.charretier@ynov.com>
2022-09-12 17:19:06 +0200

    ...

--- strlen.go
+++ strlen.go
@@ -1,5 +1,13 @@
 package piscine
 
 func StrLen(s string) int {
-	return len(s)
+	var o int
+	for i := 1; i < len(s); i++ {
+		if (rune(s[i])) == 96 {
+			o = len([]rune(s)) - 1
+		} else {
+			o = len([]rune(s))
+		}
+	}
+	return o
 }
