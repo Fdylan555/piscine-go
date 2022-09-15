d368b6bc3c0e3e4c4aa696483bece8f77eed0eb5
cdylan <dylan.charretier@ynov.com>
2022-09-15 17:33:55 +0200

    q

---
+++ trimatoi.go
@@ -0,0 +1,27 @@
+package piscine
+
+func TrimAtoi(s string) int {
+    w := []rune(s)
+    var x int
+    var y bool
+    var z int
+    for i := 0; i < len(s); i++ {
+        if w[i] >= 48 && w[i] <= 57 {
+            i = len(s)
+        } else if w[i] == 45 {
+            y = true
+        }
+    }
+    for j := 0; j < len(s); j++ {
+        if w[j] >= 48 && w[j] <= 57 {
+            x *= 10
+            x += (int(w[j] - 48))
+        }
+    }
+    if y == true {
+        z = x * 2
+        x -= z
+    }
+
+    return x
+}
