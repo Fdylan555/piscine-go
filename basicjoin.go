9afb647341c3edbea0c41ebaba73eeed6fda0d0e
cdylan <dylan.charretier@ynov.com>
2022-09-15 15:31:40 +0200

    c

--- basicjoin.go
+++ basicjoin.go
@@ -2,10 +2,8 @@ package piscine
 
 func BasicJoin(elems []string) string {
 	var a string
-	for i := 0; i <= len(elems); i++ {
-		if i >= 0 && i <= 3 {
-			a = elems[0] + elems[1] + elems[2] + elems[3]
-		}
+	for i := 0; i < len(elems); i++ {
+		a += elems[i]
 	}
 	return a
 }
