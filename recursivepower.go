cb6da31e813025fd2658a61f7fbff359c7a36a50
cdylan <dylan.charretier@ynov.com>
2022-09-19 17:20:47 +0200

    l

--- recursivepower.go
+++ recursivepower.go
@@ -3,8 +3,8 @@ package piscine
 func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
-	}else if power == 0 {
+	} else if power == 0 {
 		return 1
 	}
-	return nb * RecursivePower(nb,power-1)
+	return nb * RecursivePower(nb, power-1)
 }
