a1f7725091d3803b73e947bfcca16bb34d608380
cdylan <dylan.charretier@ynov.com>
2022-09-19 10:34:25 +0200

    g

--- capitalize.go
+++ capitalize.go
@@ -7,9 +7,6 @@ func Capitalize(s string) string {
 			x[i+1] -= 32
 		} else if i == 0 && x[0] >= 97 && x[0] <= 122 {
 			x[0] -= 32
-			}else x[i] >= 32 && x[i] <= 47 && x[i] >= 58 && x[i] <= 64 && x[i] >= 91 && x[i] <= 96 && x[i] >= 123 && x[i] <= 127 {
-				x[i] >= 65 || x[i] <= 90
-			}
 		}
 	}
 	return (string(x))
