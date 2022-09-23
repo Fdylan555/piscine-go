3f4e8f71a24a52aafe253842fdd8f112d1f65eb1
cdylan <dylan.charretier@ynov.com>
2022-09-23 10:13:10 +0200

    test

--- strrev.go
+++ strrev.go
@@ -3,8 +3,8 @@ package piscine
 func StrRev(s string) string {
 	var str string
 	z := len(s)
-	for i := z - 1; i >= 0; i-- {
-		str += (string(rune(s[i])))
+	for i := z; i > 0; i-- {
+		str += (string(rune(s[i-1])))
 	}
 	return s
 }
